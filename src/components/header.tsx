d1ded8e71cab7c6bc38e889d82b3bb15dc5ab15d
Tim <timothyiliya21@gmail.com>
2024-07-11 19:23:17 +0100

    v2

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+
+
+const Header = () => {
+  return (
+    <section className="flex justify-between items-center padding-x py-6">
+      <div className="flex items-center gap-x-3.5">
+        <i className="bi bi-code-slash" />
+        <p className="font-bold text-base">Nox</p>
+      </div>
+
+      <a href="mailto:timothyiliya21@gmail.com" className="text-sm px-4 lg:px-6 py-1.5 border-2 border-white rounded-2xl">
+        Contact me
+      </a>
+    </section>
+  );
+};
+
+export default Header;
